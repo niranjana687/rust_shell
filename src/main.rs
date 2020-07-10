60fcc89caad0c0dc1d4128fa4f0dead0aa98e8f1
Niranjana <niranjana687>
2020-07-10 10:32:18 +0530

    reverting

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,12 @@ use std::process::Command;
 
 
 fn main() {
-    /*let (code, stdout, stderr) = fish!("echo '{} + {}' | cat", 1, 3);
+    let (code, stdout, stderr) = fish!("echo '{} + {}' | cat", 1, 3);
     
     assert_eq!(code, 0);
     assert_eq!(&stdout[..], "1+3\n");
-    assert_eq!(&stderr[..], ""); */
+    assert_eq!(&stderr[..], ""); 
 
-    str = $(<f1.txt)
-    echo "$str" | awk 'for '
-
+    
 
 }
