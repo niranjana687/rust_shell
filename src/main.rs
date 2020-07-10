a854565f8ecff9a7bb9bd2d15a527cf9122a5ee2
Niranjana <niranjana687>
2020-07-10 10:27:35 +0530

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+#[macro_use] 
+extern crate shells;
+use std::process::Command;
+
+
+fn main() {
+    /*let (code, stdout, stderr) = fish!("echo '{} + {}' | cat", 1, 3);
+    
+    assert_eq!(code, 0);
+    assert_eq!(&stdout[..], "1+3\n");
+    assert_eq!(&stderr[..], ""); */
+
+    str = $(<f1.txt)
+    echo "$str" | awk 'for '
+
+
+}
